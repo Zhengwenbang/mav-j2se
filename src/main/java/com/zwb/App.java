707009f8b5d5c6b343094d049a01dcc93598f105
zwb <1919808921@qq.com>
2021-07-14 15:55:41 +0800

    push commit

--- src/main/java/com/zwb/App.java
+++ src/main/java/com/zwb/App.java
@@ -11,5 +11,6 @@ public class App {
         System.out.println("Hello World!4");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
